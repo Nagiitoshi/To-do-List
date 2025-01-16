7ca7d0ac0ff29000eccd81eeb956c2b37272b22b
Nagi <nagiitoshi@gmail.com>
2025-01-16 09:04:28 -0300

    First commit

---
+++ src/main/java/rastreador/spring/project_tracker/ProjectTodoListApplication.java
@@ -0,0 +1,13 @@
+package rastreador.spring.project_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTodoListApplication.class, args);
+	}
+
+}
